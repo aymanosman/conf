6ef156b1e20f7920f215679735ceb30bb946a5c7
Ayman Osman <aymano.osman@gmail.com>
2017-12-10 22:18:02 +0000

    wip

--- scripts/ios-test.sh
+++ scripts/ios-test.sh
@@ -1,4 +1,4 @@
-set -eo pipefail
+set -exo pipefail
 
 mkdir -p /tmp/xcode
 
@@ -7,9 +7,15 @@ xcodebuild \
   CODE_SIGN_IDENTITY= \
   PROVISIONING_PROFILE= \
   -sdk iphonesimulator \
-  -destination 'platform=iOS Simulator,OS=11.0,name=iPhone 7' \
+  -destination 'platform=iOS Simulator,OS=11.2,name=iPhone 7' \
   -project ios/conf.xcodeproj \
   -scheme "conf" \
-  clean build test | \
+  build test | \
   tee /tmp/xcode_raw.log | \
   xcpretty --color --report junit --output /tmp/xcode/results.xml
+
+tree -L 3 ios > /tmp/tree
+
+cat /tmp/tree
+
+diff -Naur .commands/tree-`git rev-parse --short HEAD` /tmp/tree
