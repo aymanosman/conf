990444230eb5266c132a0da548bddedb7a854788
Ayman Osman <aymano.osman@gmail.com>
2017-12-10 21:02:14 +0000

    tests

---
+++ scripts/ios-test.sh
@@ -0,0 +1,15 @@
+set -eo pipefail
+
+mkdir -p /tmp/xcode
+
+xcodebuild \
+  CODE_SIGNING_REQUIRED=NO \
+  CODE_SIGN_IDENTITY= \
+  PROVISIONING_PROFILE= \
+  -sdk iphonesimulator \
+  -destination 'platform=iOS Simulator,OS=11.0,name=iPhone 7' \
+  -project ios/conf.xcodeproj \
+  -scheme "conf" \
+  clean build test | \
+  tee /tmp/xcode_raw.log | \
+  xcpretty --color --report junit --output /tmp/xcode/results.xml
