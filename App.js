9e5806e77d8966e47c5b3153d8d6733b1da66e20
Ayman Osman <aymano.osman@gmail.com>
2017-12-19 20:40:42 +0000

    nav stuff

--- App.js
+++ App.js
@@ -12,40 +12,94 @@ import {
   TabNavigator,
   NavigationActions,
   addNavigationHelpers,
+  TabBarTop,
 } from 'react-navigation';
 
-const Login = props => {
-  return <Text>login screen</Text>;
+const Login = ({ navigation }) => {
+  console.log('XXX navigation', navigation);
+  return (
+    <View style={styles.container}>
+      <Text>login screen</Text>
+    </View>
+  );
 };
 
 const One = props => {
-  return <View style={styles.container}><Text>tab one</Text></View>;
+  return (
+    <View style={styles.container}>
+      <Text>tab one</Text>
+    </View>
+  );
 };
 
 const Two = props => {
-  return <View style={styles.container}><Text>tab two</Text></View>;
+  return (
+    <View style={styles.container}>
+      <Text>tab two</Text>
+    </View>
+  );
 };
 
 const Three = props => {
-  return <View style={styles.container}><Text>tab three</Text></View>;
+  return (
+    <View style={styles.container}>
+      <Text>tab three</Text>
+    </View>
+  );
 };
 
 const Main = TabNavigator({
-  One: { screen: One },
-  Two: { screen: Two },
-  Three: { screen: Three },
+  Home: {
+    screen: StackNavigator({
+      'Home/tabs': {
+        screen: TabNavigator(
+          {
+            'Home/tabs/a': {
+              screen: One,
+              navigationOptions: {
+                title: 'a',
+              },
+            },
+            'Home/tabs/b': {
+              screen: One,
+              navigationOptions: {
+                title: 'b',
+              },
+            },
+            'Home/tabs/c': {
+              screen: One,
+              navigationOptions: {
+                title: 'c',
+              },
+            },
+          },
+          {
+            tabBarPosition: 'top',
+            tabBarComponent: TabBarTop,
+          },
+        ),
+        navigationOptions: {
+          title: 'Home',
+          header: null,
+        },
+      },
+    }),
+  },
+  Chat: { screen: Two },
 });
 
-const Stack = StackNavigator({
-  Login: { screen: Login, navigationOptions: { params: { foo: 32 } } },
-  Main: {
-    screen: Main,
+const Stack = StackNavigator(
+  {
+    Login: { screen: Login },
+    Main: { screen: Main },
+  },
+  {
     navigationOptions: {
       header: null,
       gesturesEnabed: false,
     },
   },
-});
+);
 
 const Nav = connect(({ nav }) => ({ nav }))(({ dispatch, nav }) => {
   const navigation = addNavigationHelpers({
@@ -72,7 +126,7 @@ const store = createStore(reducers, applyMiddleware(thunk));
 
 setTimeout(() => {
   store.dispatch(NavigationActions.navigate({ routeName: 'Main' }));
-}, 2000);
+}, 1000);
 
 export default class App extends Component<{}> {
   render() {
