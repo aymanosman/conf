6d2a6568e122f5825be4ad19d2d80483c6de4793
Ayman Osman <aymano.osman@gmail.com>
2017-12-20 02:33:54 +0000

    fix broke

--- App.js
+++ App.js
@@ -49,7 +49,23 @@ const Three = props => {
 };
 
 setTimeout(() => {
-  Alert.alert(42);
+  Alert.alert(
+    'Alert Title',
+    'My Alert Msg',
+    [
+      {
+        text: 'Ask me later',
+        onPress: () => console.log('Ask me later pressed'),
+      },
+      {
+        text: 'Cancel',
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'cancel',
+      },
+      { text: 'OK', onPress: () => console.log('OK Pressed') },
+    ],
+    { cancelable: false },
+  );
 }, 5000);
 
 const Main = TabNavigator({
